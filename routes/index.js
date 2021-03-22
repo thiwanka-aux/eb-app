47255d4edd5f35467dcf535ea792f3df5ae94e4f
thiwanka wickramage <thiwankawickramage@gmail.com>
2021-03-22 10:25:22 +0530

    added s3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'REGION'});
+
+// Create S3 service object
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Working...' });
+router.get('/',async (req, res, next) => {
+  try {
+    const data  = await s3.listBuckets().promise();
+    res.json({title:'working', message: 'response from docusign testing server', data});
+  }catch ( e ) {
+    res.json({error: e});
+  }
 });
 
 module.exports = router;
