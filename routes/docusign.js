d0eb9e5d2b45aa3133b3b6d88539cf388bbf9bb1
thiwanka wickramage <thiwankawickramage@gmail.com>
2021-03-22 11:17:51 +0530

    updated

---
+++ routes/docusign.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'eu-west-2'});
+
+// Create S3 service object
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const router = express.Router();
+/* GET users listing. */
+router.post('/', async (req, res, next) => {
+  try {
+    console.log('req log >>>>>>>', req);
+    res.send('respond with a resource');
+  }catch ( e ) {
+    res.send(e);
+  }
+
+});
+
+module.exports = router;
