9f0b861f06ffedd896aaaf5c648e66046531f5e5
thiwanka wickramage <thiwankawickramage@gmail.com>
2021-03-22 10:28:26 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 // Set the region
-AWS.config.update({region: 'REGION'});
+AWS.config.update({region: 'eu-west-2'});
 
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
