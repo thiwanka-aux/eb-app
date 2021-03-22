849c1c5797e1afe96ac541a3d238526762a8bb13
thiwanka wickramage <thiwankawickramage@gmail.com>
2021-03-22 12:38:58 +0530

    updated

--- routes/docusign.js
+++ routes/docusign.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const AWS = require('aws-sdk');
+const xmlparser = require ('express-xml-bodyparser');
+const xml = require ('xml2js');
+
 // Set the region
 AWS.config.update({region: 'eu-west-2'});
+
+const xmlOptions = {
+  charkey: 'value',
+  trim: false,
+  explicitRoot: false,
+  explicitArray: false,
+  normalizeTags: false,
+  mergeAttrs: true,
+};
 
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const router = express.Router();
-/* GET users listing. */
-router.post('/', async (req, res, next) => {
+
+const bustHeaders = (request, response, next) => {
+  request.app.isXml = false;
+
+  if (request.headers['content-type'] === 'application/xml'
+      || request.headers['accept'] === 'application/xml'
+  ) {
+    request.app.isXml = true;
+  }
+
+  next();
+};
+
+router.post('/', bustHeaders, xmlparser(xmlOptions), async (req, res, next) => {
   try {
     console.log('req log >>>>>>>', JSON.stringify(req.body));
     res.send('respond with a resource');
   }catch ( e ) {
     res.send(e);
   }
-
 });
 
 module.exports = router;
